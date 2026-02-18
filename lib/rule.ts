6edc95b8bb32f70abb6a121aa1526452db2f961e
WatIzDat <lily3575@hotmail.com>
2026-02-17 21:12:36 -0800

    Implement environment word boundaries

--- lib/rule.ts
+++ lib/rule.ts
@@ -9,22 +9,79 @@ export function applyRule(rule: Rule, word: string): string {
         return word.replaceAll(rule.pattern, rule.replacement);
     }
 
-    const environment = rule.environment.split(" ");
+    const environment: (string | null)[] = rule.environment.split(" ");
 
-    let re: RegExp;
-
-    if (environment.length === 3) {
-        re = new RegExp(
-            `(?<=${environment[0]})${rule.pattern}(?=${environment[2]})`,
-            "g",
-        );
-    } else {
+    if (environment.length < 3) {
         if (environment[1] === "_") {
-            re = new RegExp(`(?<=${environment[0]})${rule.pattern}`, "g");
+            environment.push(null);
         } else {
-            re = new RegExp(`${rule.pattern}(?=${environment[1]})`, "g");
+            environment.unshift(null);
         }
     }
+
+    let left: string = "";
+    let right: string = "";
+
+    // if (environment.length === 3) {
+    //     // re = new RegExp(
+    //     //     `(?<=${environment[0]})${rule.pattern}(?=${environment[2]})`,
+    //     //     "g",
+    //     // );
+
+    //     if (environment[0][0] === "#") {
+    //         if (environment[0].length > 1) {
+    //             left = `^(?<=${environment[0].substring(1)})`;
+    //         } else {
+    //             left = `^`;
+    //         }
+    //     } else {
+    //         left = `(?<=${environment[0]})`;
+    //     }
+
+    //     if (environment[2][0] === "#") {
+    //         if (environment[2].length > 1) {
+    //             right = `(?=${environment[2].substring(1)})$`;
+    //         } else {
+    //             right = `$`;
+    //         }
+    //     } else {
+    //         right = `(?=${environment[2]})`;
+    //     }
+    // } else {
+    //     if (environment[1] === "_") {
+    //         re = new RegExp(`(?<=${environment[0]})${rule.pattern}`, "g");
+    //     } else {
+    //         re = new RegExp(`${rule.pattern}(?=${environment[1]})`, "g");
+    //     }
+    // }
+
+    if (environment[0]) {
+        if (environment[0][0] === "#") {
+            if (environment[0].length > 1) {
+                left = `(?<=^${environment[0].substring(1)})`;
+            } else {
+                left = `^`;
+            }
+        } else {
+            left = `(?<=${environment[0]})`;
+        }
+    }
+
+    if (environment[2]) {
+        if (environment[2][environment[2].length - 1] === "#") {
+            if (environment[2].length > 1) {
+                right = `(?=${environment[2].substring(0, environment[2].length - 1)}$)`;
+            } else {
+                right = `$`;
+            }
+        } else {
+            right = `(?=${environment[2]})`;
+        }
+    }
+
+    const re: RegExp = new RegExp(`${left}${rule.pattern}${right}`, "g");
+
+    console.log(re);
 
     return word.replace(re, rule.replacement);
 }
