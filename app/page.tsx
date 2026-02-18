2b5e5b9dbf86749fa7599b0ae17c376349830f2e
WatIzDat <lily3575@hotmail.com>
2026-02-18 15:36:28 -0800

    Make home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import LevelPage from "@/components/level";
-import { Level } from "@/lib/level";
+import Link from "next/link";
 
 export default async function Home() {
-    // const response = await fetch("http://localhost:3000/levels/level1.json");
-    // const level: Level = await response.json();
-    // console.log(level);
-    // return <LevelPage level={level} />;
+    const numLevels = 15;
+
+    return (
+        <main className="flex flex-col gap-6 h-screen w-screen items-center justify-center p-12 text-3xl">
+            <header className="text-center">
+                <h1 className="text-9xl font-light mb-6">chain→ling</h1>
+                <p className="font-extralight">
+                    the puzzle game about sound change
+                </p>
+            </header>
+            <Link href="/1">play</Link>
+            <div className="mt-6">
+                <h2 className="font-extralight mb-6">levels</h2>
+                <div className="grid grid-cols-4 grid-rows-4 gap-6">
+                    {[...Array(numLevels)].map((_, i) => (
+                        <Link key={i} href={`/${i + 1}`}>
+                            {i + 1}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </main>
+    );
 }
